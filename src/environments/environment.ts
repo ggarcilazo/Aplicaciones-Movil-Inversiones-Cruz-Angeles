b53d84a1fddfc643085a28f53d4c2ce20dabcbbf
Giovanni Joaquin Garcilazo Lopez <164566841+ggarcilazo@users.noreply.github.com>
2025-07-24 19:55:40 -0500

    Update environment.ts
    Correcion de clave

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDrFVhN9H-hs773WFhtPy-R5-y1sW6dTL8",
+    apiKey: "----", //Utilice su propio apiKey de google que ustedes deba utilizar para su base de datos
     authDomain: "inversiones-cruz-angeles.firebaseapp.com",
     projectId: "inversiones-cruz-angeles",
     storageBucket: "inversiones-cruz-angeles.firebasestorage.app",
-    messagingSenderId: "1084044333743",
-    appId: "1:1084044333743:web:e80e1d07275ebbcf0eba5a",
-    measurementId: "G-1H51VJ0PTT"
+    messagingSenderId: "-----",
+    appId: "-----", //Utilice su propio apiKey de google que ustedes deba utilizar para su base de datos
+    measurementId: "----"
   }
 };
 
